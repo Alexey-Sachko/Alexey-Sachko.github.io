9a7ab000904b2fed72695f93f59475fd9f905480
Alexey Sachko <alexey.sachko@gmail.com>
2019-02-21 00:06:17 +0300

    second

---
+++ precache-manifest.5b11aa33b2b04b0b139a4e9f286afd81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "07be14a5715e24dd3a4d",
+    "url": "/static/css/main.33f73da7.chunk.css"
+  },
+  {
+    "revision": "07be14a5715e24dd3a4d",
+    "url": "/static/js/main.07be14a5.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d7a4663edba1b43342b7",
+    "url": "/static/js/2.d7a4663e.chunk.js"
+  },
+  {
+    "revision": "7746e63c70d23b0bafb3c62cd77dbf07",
+    "url": "/index.html"
+  }
+];
